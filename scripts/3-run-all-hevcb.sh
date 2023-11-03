ffe2eeb7ab761a065fdf9095d79df071565820d1
Nguyễn Tống Minh <86721208+minhngt62@users.noreply.github.com>
2023-11-03 13:11:11 +0700

    Update output dirs for HEVC-B

--- scripts/3-run-all-hevcb.sh
+++ scripts/3-run-all-hevcb.sh
@@ -4,25 +4,25 @@
 mkdir -p data/hevc_b/BQTerrace_1920x1080_60
 ffmpeg -pix_fmt yuv420p -s 1920x1080 -i data/BQTerrace_1920x1080_60.yuv -f image2 data/hevc_b/BQTerrace_1920x1080_60/f%05d.png
 
-python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/BQTerrace_1920x1080_60" --vid beauty \
+python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/BQTerrace_1920x1080_60" --vid bqt \
     --conv_type convnext pshuffel --act gelu --norm none  --crop_list 1080_1920  \
     --resize_list -1 --loss Fusion6  --enc_strds 5 3 2 2 2 --enc_dim 64_16 \
     --dec_strds 5 3 2 2 2 --ks 0_1_5 --reduce 1.2 --dump_images \
     --modelsize 3 -e 300 --eval_freq 150 --lower_width 12 -b 2 --lr 0.001
 
-python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/BQTerrace_1920x1080_60" --vid beauty \
+python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/BQTerrace_1920x1080_60" --vid bqt \
     --conv_type convnext pshuffel --act gelu --norm none  --crop_list 1080_1920  \
     --resize_list -1 --loss Fusion6  --enc_strds 5 3 2 2 2 --enc_dim 64_16 \
     --dec_strds 5 3 2 2 2 --ks 0_1_5 --reduce 1.2 --dump_images \
     --modelsize 6 -e 300 --eval_freq 150 --lower_width 12 -b 2 --lr 0.001
 
-python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/BQTerrace_1920x1080_60" --vid beauty \
+python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/BQTerrace_1920x1080_60" --vid bqt \
     --conv_type convnext pshuffel --act gelu --norm none  --crop_list 1080_1920  \
     --resize_list -1 --loss Fusion6  --enc_strds 5 3 2 2 2 --enc_dim 64_16 \
     --dec_strds 5 3 2 2 2 --ks 0_1_5 --reduce 1.2 --dump_images \
     --modelsize  -e 300 --eval_freq 150 --lower_width 12 -b 2 --lr 0.001
 
-python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/BQTerrace_1920x1080_60" --vid beauty \
+python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/BQTerrace_1920x1080_60" --vid bqt \
     --conv_type convnext pshuffel --act gelu --norm none  --crop_list 1080_1920  \
     --resize_list -1 --loss Fusion6  --enc_strds 5 3 2 2 2 --enc_dim 64_16 \
     --dec_strds 5 3 2 2 2 --ks 0_1_5 --reduce 1.2 --dump_images \
@@ -32,25 +32,25 @@ python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/
 mkdir -p data/hevc_b/BasketballDrive_1920x1080_50
 ffmpeg -pix_fmt yuv420p -s 1920x1080 -i data/BasketballDrive_1920x1080_50.yuv -f image2 data/hevc_b/BasketballDrive_1920x1080_50/f%05d.png
 
-python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/BasketballDrive_1920x1080_50" --vid honey \
+python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/BasketballDrive_1920x1080_50" --vid basket \
     --conv_type convnext pshuffel --act gelu --norm none  --crop_list 1080_1920  \
     --resize_list -1 --loss Fusion6  --enc_strds 5 3 2 2 2 --enc_dim 64_16 \
     --dec_strds 5 3 2 2 2 --ks 0_1_5 --reduce 1.2 --dump_images \
     --modelsize 3 -e 300 --eval_freq 150 --lower_width 12 -b 2 --lr 0.001
 
-python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/BasketballDrive_1920x1080_50" --vid honey \
+python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/BasketballDrive_1920x1080_50" --vid basket \
     --conv_type convnext pshuffel --act gelu --norm none  --crop_list 1080_1920  \
     --resize_list -1 --loss Fusion6  --enc_strds 5 3 2 2 2 --enc_dim 64_16 \
     --dec_strds 5 3 2 2 2 --ks 0_1_5 --reduce 1.2 --dump_images \
     --modelsize 6 -e 300 --eval_freq 150 --lower_width 12 -b 2 --lr 0.001
 
-python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/BasketballDrive_1920x1080_50" --vid honey \
+python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/BasketballDrive_1920x1080_50" --vid basket \
     --conv_type convnext pshuffel --act gelu --norm none  --crop_list 1080_1920  \
     --resize_list -1 --loss Fusion6  --enc_strds 5 3 2 2 2 --enc_dim 64_16 \
     --dec_strds 5 3 2 2 2 --ks 0_1_5 --reduce 1.2 --dump_images \
     --modelsize 9 -e 300 --eval_freq 150 --lower_width 12 -b 2 --lr 0.001
 
-python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/BasketballDrive_1920x1080_50" --vid honey \
+python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/BasketballDrive_1920x1080_50" --vid basket \
     --conv_type convnext pshuffel --act gelu --norm none  --crop_list 1080_1920  \
     --resize_list -1 --loss Fusion6  --enc_strds 5 3 2 2 2 --enc_dim 64_16 \
     --dec_strds 5 3 2 2 2 --ks 0_1_5 --reduce 1.2 --dump_images \
@@ -60,25 +60,25 @@ python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/
 mkdir -p data/hevc_b/Cactus_1920x1080_50
 ffmpeg -pix_fmt yuv420p -s 1920x1080 -i data/Cactus_1920x1080_50.yuv -f image2 data/hevc_b/Cactus_1920x1080_50/f%05d.png
 
-python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/Cactus_1920x1080_50" --vid bosphorus \
+python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/Cactus_1920x1080_50" --vid cactus \
     --conv_type convnext pshuffel --act gelu --norm none  --crop_list 1080_1920  \
     --resize_list -1 --loss Fusion6  --enc_strds 5 3 2 2 2 --enc_dim 64_16 \
     --dec_strds 5 3 2 2 2 --ks 0_1_5 --reduce 1.2 --dump_images \
     --modelsize 3 -e 300 --eval_freq 150 --lower_width 12 -b 2 --lr 0.001
 
-python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/Cactus_1920x1080_50" --vid bosphorus \
+python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/Cactus_1920x1080_50" --vid cactus \
     --conv_type convnext pshuffel --act gelu --norm none  --crop_list 1080_1920  \
     --resize_list -1 --loss Fusion6  --enc_strds 5 3 2 2 2 --enc_dim 64_16 \
     --dec_strds 5 3 2 2 2 --ks 0_1_5 --reduce 1.2 --dump_images \
     --modelsize 6 -e 300 --eval_freq 150 --lower_width 12 -b 2 --lr 0.001
 
-python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/Cactus_1920x1080_50" --vid bosphorus \
+python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/Cactus_1920x1080_50" --vid cactus \
     --conv_type convnext pshuffel --act gelu --norm none  --crop_list 1080_1920  \
     --resize_list -1 --loss Fusion6  --enc_strds 5 3 2 2 2 --enc_dim 64_16 \
     --dec_strds 5 3 2 2 2 --ks 0_1_5 --reduce 1.2 --dump_images \
     --modelsize 9 -e 300 --eval_freq 150 --lower_width 12 -b 2 --lr 0.001
 
-python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/Cactus_1920x1080_50" --vid bosphorus \
+python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/Cactus_1920x1080_50" --vid cactus \
     --conv_type convnext pshuffel --act gelu --norm none  --crop_list 1080_1920  \
     --resize_list -1 --loss Fusion6  --enc_strds 5 3 2 2 2 --enc_dim 64_16 \
     --dec_strds 5 3 2 2 2 --ks 0_1_5 --reduce 1.2 --dump_images \
@@ -88,25 +88,25 @@ python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/
 mkdir -p data/hevc_b/Kimono_1920x1080_24
 ffmpeg -pix_fmt yuv420p -s 1920x1080 -i data/Kimono_1920x1080_24.yuv -f image2 data/hevc_b/Kimono_1920x1080_24/f%05d.png
 
-python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/Kimono_1920x1080_24" --vid jockey \
+python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/Kimono_1920x1080_24" --vid kimono \
     --conv_type convnext pshuffel --act gelu --norm none  --crop_list 1080_1920  \
     --resize_list -1 --loss Fusion6  --enc_strds 5 3 2 2 2 --enc_dim 64_16 \
     --dec_strds 5 3 2 2 2 --ks 0_1_5 --reduce 1.2 --dump_images \
     --modelsize 3 -e 300 --eval_freq 150 --lower_width 12 -b 2 --lr 0.001
 
-python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/Kimono_1920x1080_24" --vid jockey \
+python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/Kimono_1920x1080_24" --vid kimono \
     --conv_type convnext pshuffel --act gelu --norm none  --crop_list 1080_1920  \
     --resize_list -1 --loss Fusion6  --enc_strds 5 3 2 2 2 --enc_dim 64_16 \
     --dec_strds 5 3 2 2 2 --ks 0_1_5 --reduce 1.2 --dump_images \
     --modelsize 6 -e 300 --eval_freq 150 --lower_width 12 -b 2 --lr 0.001
 
-python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/Kimono_1920x1080_24" --vid jockey \
+python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/Kimono_1920x1080_24" --vid kimono \
     --conv_type convnext pshuffel --act gelu --norm none  --crop_list 1080_1920  \
     --resize_list -1 --loss Fusion6  --enc_strds 5 3 2 2 2 --enc_dim 64_16 \
     --dec_strds 5 3 2 2 2 --ks 0_1_5 --reduce 1.2 --dump_images \
     --modelsize 9 -e 300 --eval_freq 150 --lower_width 12 -b 2 --lr 0.001
 
-python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/Kimono_1920x1080_24" --vid jockey \
+python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/Kimono_1920x1080_24" --vid kimono \
     --conv_type convnext pshuffel --act gelu --norm none  --crop_list 1080_1920  \
     --resize_list -1 --loss Fusion6  --enc_strds 5 3 2 2 2 --enc_dim 64_16 \
     --dec_strds 5 3 2 2 2 --ks 0_1_5 --reduce 1.2 --dump_images \
@@ -116,25 +116,25 @@ python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/
 mkdir -p data/hevc_b/ParkScene_1920x1080_24
 ffmpeg -pix_fmt yuv420p -s 1920x1080 -i data/ParkScene_1920x1080_24.yuv -f image2 data/hevc_b/ParkScene_1920x1080_24/f%05d.png
 
-python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/ParkScene_1920x1080_24" --vid ready \
+python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/ParkScene_1920x1080_24" --vid park \
     --conv_type convnext pshuffel --act gelu --norm none  --crop_list 1080_1920  \
     --resize_list -1 --loss Fusion6  --enc_strds 5 3 2 2 2 --enc_dim 64_16 \
     --dec_strds 5 3 2 2 2 --ks 0_1_5 --reduce 1.2 --dump_images \
     --modelsize 3 -e 300 --eval_freq 150 --lower_width 12 -b 2 --lr 0.001
 
-python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/ParkScene_1920x1080_24" --vid ready \
+python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/ParkScene_1920x1080_24" --vid park \
     --conv_type convnext pshuffel --act gelu --norm none  --crop_list 1080_1920  \
     --resize_list -1 --loss Fusion6  --enc_strds 5 3 2 2 2 --enc_dim 64_16 \
     --dec_strds 5 3 2 2 2 --ks 0_1_5 --reduce 1.2 --dump_images \
     --modelsize 6 -e 300 --eval_freq 150 --lower_width 12 -b 2 --lr 0.001
 
-python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/ParkScene_1920x1080_24" --vid ready \
+python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/ParkScene_1920x1080_24" --vid park \
     --conv_type convnext pshuffel --act gelu --norm none  --crop_list 1080_1920  \
     --resize_list -1 --loss Fusion6  --enc_strds 5 3 2 2 2 --enc_dim 64_16 \
     --dec_strds 5 3 2 2 2 --ks 0_1_5 --reduce 1.2 --dump_images \
     --modelsize 9 -e 300 --eval_freq 150 --lower_width 12 -b 2 --lr 0.001
 
-python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/ParkScene_1920x1080_24" --vid ready \
+python "HNeRV/train_nerv_all.py"  --outf "checkpoint"  --data_path "data/hevc_b/ParkScene_1920x1080_24" --vid park \
     --conv_type convnext pshuffel --act gelu --norm none  --crop_list 1080_1920  \
     --resize_list -1 --loss Fusion6  --enc_strds 5 3 2 2 2 --enc_dim 64_16 \
     --dec_strds 5 3 2 2 2 --ks 0_1_5 --reduce 1.2 --dump_images \
